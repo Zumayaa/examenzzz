874c0dfd5a0749005dae2c2b390c69a605535192
Marcos <magma_ruby145@hotmail.com>
2023-10-08 15:20:14 -0700

    Navbar a otras vistas y la cosa de arriba ya tiene 283 idiomas

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,8 +1,6 @@
-
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
-
-menu_btn.addEventListener('click', function () {
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+const mobile_search = document.querySelector('#search-m-container');
+const search_button = document.querySelector('#search-button')
+search_button.addEventListener('click', function () {
+    mobile_search.classList.toggle('is-active');
+    search_button.classList.toggle('is-active');
 });
