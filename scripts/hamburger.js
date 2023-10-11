5b50908f2e2ae44b063a94aaec6c8e650edd2169
Marcos <magma_ruby145@hotmail.com>
2023-10-10 22:59:00 -0700

    Confirmar compra

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -4,3 +4,6 @@ search_button.addEventListener('click', function () {
     mobile_search.classList.toggle('is-active');
     search_button.classList.toggle('is-active');
 });
+document.getElementById("buy-button").addEventListener("click", function() {
+    window.location.href = "purchase-confirmation.html"; // Reemplaza "https://www.ejemplo.com" con la URL a la que deseas redirigir.
+});
